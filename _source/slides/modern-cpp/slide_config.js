37c8a1db54e3334dbc3058215d7fdbb33b886c0e
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-12-09 23:22:31 +0000

    Add the C++ slides

---
+++ _source/slides/modern-cpp/slide_config.js
@@ -0,0 +1,23 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Modern C++',
+    subtitle: '',
+    useBuilds: false, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: '',
+  },
+
+  // Author information
+  presenters: [{
+    name: '<a href="http://twitter.com/klmr">@klmr</a>',
+    company: '',
+    gplus: '',
+    twitter: '@klmr',
+    www: '//klmr.me',
+    github: 'http://github.com/klmr'
+  }]
+};
+
